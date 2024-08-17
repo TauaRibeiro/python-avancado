0dbb2aded5ca9616f4deaf0ff73cb0809a6abf67
TauaRibeiro <166765664+TauaRibeiro@users.noreply.github.com>
2024-08-16 21:16:56 -0300

    Update lista invertida.py

--- lista invertida.py
+++ lista invertida.py
@@ -1,6 +1,14 @@
-# Crie uma classe chamada ListaInvertida que encapsula uma lista. Implemente os seguintes métodos dunder:
+class ListaInvertida:
+    def __init__(self, lista):
+        self.lista = lista[::]
 
-# __init__(self, lista): Inicializa a classe com uma lista.
-# __repr__(self): Retorna uma representação da lista invertida no formato ListaInvertida([elementos]).
-# __getitem__(self, index): Permite acessar os elementos da lista de forma invertida (por exemplo, lista_invertida[0] deve retornar o último elemento da lista original).
-# __len__(self): Retorna o comprimento da lista.
+    def __repr__(self):
+        return f'{self.lista}'
+
+
+    def __getitem__(self, index):
+        return self.lista[len(self.lista)-index-1]
+
+    def __len__(self):
+        return len(self.lista)
+
