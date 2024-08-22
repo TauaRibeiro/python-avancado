90e9b6e479be42523a5bb3c66cee6f09745395e2
Tauã <tataumani@gmail.com>
2024-08-22 08:04:15 -0300

    update extração de dados

--- extração de dados.py
+++ extração de dados.py
@@ -4,9 +4,24 @@ texto  =  'Contato: email1@dominio.com, email2@outrodominio.com. Pagamentos: R$ 
 
 
 emails = re.findall(r'\w+@\w+\.\w{3}', texto)
-pagamentos = re.findall(r'R\$\s?(?:\d{1,3}\.)*\d{1,3},\d{2}', texto)
-data = re.findall(r'(\d{2})/(\d{2})/(\d{4})',texto)
+pagamentos = re.findall(r'R\$\s?((?:\d{1,3}\.)*\d{1,3}),(\d{2})', texto)
+datas = re.findall(r'(\d{2})/(\d{2})/(\d{4})',texto)
 
-print(emails)
-print(pagamentos)
-print(data)
+datas_novas = list()
+pagamentos_novos = list()
+
+for i in datas:
+    datas_novas.append(f'{i[2]}-{i[1]}-{i[0]}')
+
+for i in pagamentos:
+    inteiros = i[0]
+    decimal = i[1]
+
+    inteiros = inteiros.replace('.', '')
+
+    pagamentos_novos.append(float((inteiros+'.'+decimal)))
+
+
+print(f'emails = {emails}')
+print(f'datas = {datas_novas}')
+print(f'valores = {pagamentos_novos}')
