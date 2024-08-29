9d679cfe669f9c7a4c4611feadb426836b028691
Tauã <tataumani@gmail.com>
2024-08-29 17:46:08 -0300

    finalizando verificador de expressões

--- Verificador_expressões.py
+++ Verificador_expressões.py
@@ -1,13 +1,46 @@
-# A terminar
-
 def balanceada(expressao):
     pilha = list()
+    cont_parenteses = cont_colchetes = cont_chaves = 0
 
     for elemento in expressao:
         if elemento in '()[]{}':
             pilha.append(elemento)
 
-    print(pilha)
+    for i in range(0,len(pilha)):
+        elemento = pilha.pop()
+        
+        if elemento in '()':
+            if elemento == '(' and cont_parenteses == 0:
+                return False
+            elif elemento == ')':
+                cont_parenteses += 1
+            else:
+                cont_parenteses -= 1
 
+        if elemento in '[]':
+            if elemento == '[' and cont_colchetes == 0:
+                return False
+            elif elemento == ']':
+                cont_colchetes += 1
+            else:
+                cont_colchetes -= 1
 
-balanceada('(2*3)')
+        if elemento in '{}':
+            if elemento == '{' and cont_chaves == 0:
+                return False
+            elif elemento == '}':
+                cont_chaves += 1
+            else:
+                cont_chaves -= 1
+        
+    if cont_parenteses == cont_colchetes == cont_chaves == 0:
+        return True
+    else:
+        return False
+
+try:
+    print(balanceada(input('Digite uma expressão: (ex: {[(2+3)*4]/3}*5): ')))
+except KeyboardInterrupt:
+    print('\nNenhuma expressão foi digitada...')
+except Exception.__class__ as erro:
+    print('O erro foi -> '+erro)
