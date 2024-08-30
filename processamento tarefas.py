aba97c74f1e6f012d4baf60723a8bb7a47c20ada
Tauã <tataumani@gmail.com>
2024-08-30 17:49:55 -0300

    salvando codigo

--- processamento tarefas.py
+++ processamento tarefas.py
@@ -1,25 +1,60 @@
-def processamento(tarefas):
-    fila = ([],[],[])
+from time import sleep
+
+def processar(tarefas):
+    fila = []
+    buffer = []
 
     for elemento in tarefas:
-        fila[elemento['prioridade']-1].append(elemento['tarefa'])
+        buffer.append(elemento['tarefa'])
+        buffer.append(elemento['prioridade'])
 
+        if len(fila) == 0:
+            fila.append(buffer[:])
+        else:
+            for index, ordem in enumerate(fila):
+                if elemento['prioridade'] < ordem[1]:
+                    fila.insert(index, buffer[:])
+
+        buffer.clear()
+        
     print(fila)
 
-    for prioridade in fila:
-        for x in range(0, len(prioridade)):
-            print(prioridade.pop(0))
+    for elemento in fila:
+        print('REALIZANDO TAREFAS DE PRIORIDADE ' + elemento['prioridade'])
+        for x in range(len(elemento), 0):
+            print('Executando '+ elemento.pop(), flush= True)
 
-processamento([{'tarefa':"comer", 'prioridade':1},
-               {'tarefa':"jogar", 'prioridade':3},
-               {'tarefa':"dormir", 'prioridade':1},
-               {'tarefa':"assistir vídeos", 'prioridade':2},
-               {'tarefa':"tomar banho", 'prioridade':2},
-               {'tarefa':"criar o tigrinho", 'prioridade':3},
-               {'tarefa':"alisar o gato", 'prioridade':1}])
+            sleep(2)
+    print()
 
 
+entrada = dict()
+tarefas = list()
 
+while True:
+    entrada['tarefa'] = input('Digite a tarefa a ser realizada: ')
+    entrada['prioridade'] = int(input('Digite a prioridade da tarefa (1 à 3): '))
 
+    tarefas.append(entrada.copy())
+    entrada.clear()
 
+    while True:
+        parar = input('Deseja continuar? [SIM / NÃO]: ').lower()
+        
+        if parar in ['sim', 'não', 'nao', 's', 'n']:
+            if parar in ['não', 'nao', 'n']:
+                parar = True
+            else:
+                parar = False
+            break
+        
+        else:
+            print('Opção inválida!! Por favor tente novamente...')
+    
+    print('-'*30)
+
+    if parar:
+        break
+    
+processar(tarefas)    
 
