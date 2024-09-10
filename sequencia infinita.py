109ebf55b5eded47ba627f1e563e5827cffbc088
Tauã <tataumani@gmail.com>
2024-09-10 17:49:45 -0300

    entendo csv

--- sequencia infinita.py
+++ sequencia infinita.py
@@ -1,15 +1,13 @@
 def sequencia_infinita(numero):
     status = 'continuar'
-    incrementador = None
+    numero_alteracao = None
     while True:
         entrada = yield numero
-        
-        print(entrada)
-        if entrada == None:
-            # terminar essa parte
-            if status == 'pular' or status == 'reset':
-                status = ('pular', incrementador)
 
+        if entrada == None:
+            if status == 'pular' or status == 'reset':
+                status = (status, numero_alteracao)
+            
             entrada = status
 
         if entrada == 'continuar':
@@ -21,21 +19,16 @@ def sequencia_infinita(numero):
             status = 'pausar'
 
         elif entrada[0] == 'pular':
-            incrementador = entrada[1]
-            numero += incrementador
-            status = 'pular'
-
-        elif entrada[] == 'reset':
-
-
-            
-
-
-gerador = sequencia_infinita(10)
-
-print(next(gerador))
-print(next(gerador))
-
-gerador.send(('pular', 3))
-print(next(gerador))
-print(next(gerador))
+            if not numero_alteracao in entrada:
+                numero_alteracao = entrada[1]
+                numero += numero_alteracao
+                status = 'pular'
+            else:
+                status = 'continuar'
+        elif entrada[0] == 'reset':
+            if numero != numero_alteracao:
+                numero_alteracao = entrada[1]
+                numero = numero_alteracao
+                status = 'reset'
+            else:
+                status = 'continuar'
