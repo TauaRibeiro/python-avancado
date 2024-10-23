d1384d64c3f01965822fea3fa2bbf029f9b887c2
TauaRibeiro <166765664+TauaRibeiro@users.noreply.github.com>
2024-10-22 21:33:14 -0300

    Update Artigo.py (a ser testado)

--- gerenciamento_publicacoes_cientificas/src/classes/Artigo.py
+++ gerenciamento_publicacoes_cientificas/src/classes/Artigo.py
@@ -1,7 +1,7 @@
 class Artigo:
     _GERADOR_ID = 1
     lista_artigos = list()
-    def __init__(self, titulo, autores, ano_publicacao, palavras_chaves):
+    def __init__(self, titulo: str, autores: list, ano_publicacao: int, palavras_chaves: list[str]):
 
         self._titulo = titulo
         self._autores = autores
@@ -24,7 +24,23 @@ class Artigo:
 
     def __repr__(self):
         return f'Id: {self._id},Titulo: {self._titulo}, Autores: {self._autores}, Ano_publicação: {self._ano_publicacao}, palavras chaves: {self._palavras_chaves}'
-    
+
+    def setTitulo(self, titlo: str) -> bool:
+        if len(titulo) >= 4:
+            self._titulo = titulo
+            return True
+
+        return False
+
+    def setAutores(self, autores: list) -> bool:
+        if(not len(autores)):
+            return False
+
+        for autor in autores:
+            if len(autor[0]) < 4 or len(autor[1]) < 3):
+                return False
+
+        self._autores = autores
 
 if __name__ == '__main__':
     artigo1 = Artigo("olá mundo", ["Teste 1", "Teste 2"], 2024, ["olá", "mundo"])
@@ -33,4 +49,5 @@ if __name__ == '__main__':
 
     print(artigo1.__repr__)
     print(artigo2.__repr__)
+    print(artigo3.__repr__)
     print(artigo3.__repr__)
