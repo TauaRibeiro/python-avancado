adc5c7d844d1eb9e554bca6caa28893e841dbe6d
TauaRibeiro <166765664+TauaRibeiro@users.noreply.github.com>
2024-10-25 21:29:18 -0300

    Atualizando a classe autor
    métodos cadastro e mostrar testados e funcionando
    melhorar editar

--- gerenciamento_publicacoes_cientificas/src/classes/Autor.py
+++ gerenciamento_publicacoes_cientificas/src/classes/Autor.py
@@ -50,8 +50,8 @@ class Autor:
         resultado = []
 
         for autor in Autor.lista_autores:
-            resultado.append(autor.__str__)
-
+            resultado.append(str(autor))
+        
         return ''.join(resultado)
     
 
@@ -68,6 +68,12 @@ class Autor:
     def excluirAutor(indice_autor) -> None:
         Autor.lista_autores.remove(indice_autor)
         
+    @staticmethod
+    def cadastrouAutor() -> bool:
+        if len(Autor.lista_autores) == 0:
+            return False
+        return True
+        
 
 
 if __name__ == '__main__':
@@ -78,32 +84,88 @@ if __name__ == '__main__':
               '\n3- Editar',
               '\n4- Excluir',
               '\n5- Finalizar')
+        print('-'*30)
         decisao = int(input('Digite a decisão: '))
         print('='*30)
 
         match(decisao):
             case 1:
-                nome_autor = input('Digite o nome do autor')
-                instituicao_autor = input('Digite a instituicao do autor')
-
-                novo_autor = Autor(nome_autor, instituicao_autor)
-                # CORRIGIR
                 
-                # while True:
-                #     if (novo_autor.nome = nome_autor): 
+                while True: 
+                    novo_autor = Autor('Nome', 'Instituição')
+                        
+                    while True:
+                        if novo_autor.nome == 'Nome':
+                            try:
+                                novo_autor.nome = input('Digite o nome do autor: ')
+                            except ValueError:
+                                print('Nome inválido!! Deve ter no mínimo 3 caracteres.')
+                                print('Por favor, tente novamente...')
+                                
+                                continue
+                        
+                        try:
+                            novo_autor.instituicao = input('Digite o nome da instituição: ')
+                        except ValueError:
+                            print('Nome inválido!! Deve ter no mínimo 3 caracteres.')
+                            print('Por favor, tente novamente...')
+                            
+                            continue
+                        
+                        break
                     
-                #     if novo_autor.nome == nome_autor:
-                #         print('ERRO, Nome inválido!! Por favor tente novamente...')
-                #     if Autor.cadastrarAutor(novo_autor):
-                #         break
+                    break
 
+                Autor.cadastrarAutor(novo_autor)
+    
             case 2:
-                ...
+                print(Autor.mostrarAutores())
+            
             case 3:
-                ...
+                if not Autor.cadastrouAutor():
+                    print('Não há autores cadastrados...')
+                    continue
+                    
+                autor_editado = Autor('Nome', 'Instituição')
+                
+                for indice, autor in enumerate(Autor.lista_autores):
+                    print(f'{indice+1}- {autor.nome}')
+
+                print('-'*30)
+                decisao = int(input('Digite o número da opção desejada: '))-1
+                
+                while True:
+                    print('='*30)
+                    print('1- Nome',
+                        '\n2- Instituição')
+                    print('-'*30)
+                    editar = input('Digite o(os) número(os) do(os) campo(os) que deseja editar (ex: 1; 1,2): ')
+                    print('='*30)
+                    
+                    if not '1' in editar and not '2' in editar:
+                        print('Escolha inválida!! Por favor tente novamente')
+                        continue
+                    
+                    # Alterar o método editar no
+                    
+                    # while True:
+                    #     if novo_autor.nome == 'Nome':
+                    #         if '1' in editar:
+                    #             try:
+                    #                 novo_autor.nome = input('Digite o nome do autor: ')
+                    #             except ValueError:
+                    #                 print('Nome inválido!! Por favor tente novamente...')
+                    #                 continue
+                    #         else:
+                    #             novo_autor.nome = Autor.lista_autores[indice].nome
+                    #     if novo_autor.instituicao == 'Instituição' and '':
+                                   
+            
             case 4:
                 ...
+            
             case 5:
-                ...
+                break
+            
             case _:
                 ...
